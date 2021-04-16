fc55b6376c5e25a92a91f270fb9e7822e53393b8
Oleksandr Shtromberg <onestromberg@gmail.com>
2021-04-16 19:09:19 +0300

    bugfix, lockfile

--- pages/partials/header.js
+++ pages/partials/header.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import Head from 'next/head'
-export default class extends React.Component {
-  render() {
-    return [
-      <Head key={1}>
-        <link rel="stylesheet" type="text/css" href="/static/style.css"/>
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet"/>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
-        <title>{ this.props.cosmic.post ? this.props.cosmic.post.title + ' |' : '' } Simple React Blog</title>
-      </Head>,
-      <header className="header" key={2}>
-        <h1 className="site-title">
-          <a href="/">{ this.props.cosmic.global.header.metadata.site_title }</a>
-          <small className="site-title__tag">{ this.props.cosmic.global.header.metadata.site_tag }</small>
-        </h1>
-      </header>
-    ]
+export default function ({ cosmic }) {
+  if (!cosmic) {
+    return <p>Loading...</p>
   }
+  return [
+    <Head key={1}>
+      <link rel="stylesheet" type="text/css" href="/static/style.css" />
+      <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      <title>{cosmic.post ? cosmic.post.title + ' |' : ''} Simple React Blog</title>
+    </Head>,
+    <header className="header" key={2}>
+      <h1 className="site-title">
+        <a href="/">{cosmic.global.header.metadata.site_title}</a>
+        <small className="site-title__tag">{cosmic.global.header.metadata.site_tag}</small>
+      </h1>
+    </header>
+  ]
 }
